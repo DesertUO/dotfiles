1af1f14dcd6a5fc9bd4fa37ec2fbd19b829cf824
DesertUo <desert21original@gmail.com>
2025-12-09 16:34:36 -0500

    y

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ DOTFILES="$HOME/dotfiles"
 ln -s "$DOTFILES/zshrc.zshrc" "$HOME/.zshrc"
 ln -s "$DOTFILES/nvim" "$HOME/.config/nvim"
 ln -s "$DOTFILES/tmux" "$HOME/.config/tmux"
+
+chsh -s /bin/zsh
+
